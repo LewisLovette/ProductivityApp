8e6b112a915d95472e03de1e4019bd4a5722f904
Lewis Lovette <lewislovette3@hotmail.co.uk>
2018-05-04 15:15:40 +0100

    WORKS - with Obelisk.js sample cube
    This version works, wanted to upload it before I screw something up!

---
+++ platforms/browser/www/scripts.js
@@ -0,0 +1,25 @@
+document.write("Testing Scripts.js Connection")
+
+window.onload=function(){
+    // get DOM or jQuery element
+    // like: $('#canvas-demo')
+    var canvas = document.getElementById('canvas-demo');
+
+    // create pixel view container in point
+    var point = new obelisk.Point(270, 120);
+    var pixelView = new obelisk.PixelView(canvas, point);
+
+    // create dimension instance
+    var dimension = new obelisk.CubeDimension(120, 200, 60);
+    // create color instance
+    var color = new obelisk.CubeColor().getByHorizontalColor(obelisk.ColorPattern.GRAY);
+
+    // create cube primitive
+    var cube = new obelisk.Cube(dimension, color);
+    // create cube primitive wihtout border
+    //var cube = new obelisk.Cube(dimension, color, false);
+
+    // render primitive to view
+    pixelView.renderObject(cube);
+
+}
