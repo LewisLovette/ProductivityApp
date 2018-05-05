4a73726593a2eafff53e58237f333e9a322a2846
Lewis Lovette <lewislovette3@hotmail.co.uk>
2018-05-05 20:17:23 +0100

    Added color + aligned
    Learning more - Tapping out on this for today but really enjoying it so far!

--- platforms/browser/www/scripts.js
+++ platforms/browser/www/scripts.js
@@ -5,9 +5,9 @@ var y = 0;
 
 var arr = [
 	[1, 2, 3, 4, 5, 6, 7],
-	[7, 6, 5, 4, 3, 2, 1],
-	[5, 2, 6, 7, 3, 4, 2],
-	[2, 1, 5, 4, 5, 3, 6]
+	[7, 6, 5, 3, 3, 2, 1],
+	[5, 2, 6, 7, 3, 4, 0],
+	[2, 0, 5, 4, 5, 3, 6]
 ];
 
 window.onload=function(){
@@ -16,13 +16,14 @@ window.onload=function(){
     var canvas = document.getElementById('canvas-demo');
 
     // create pixel view container in point
-    var point = new obelisk.Point(130, 130);
+    var point = new obelisk.Point(110, 150);
     var pixelView = new obelisk.PixelView(canvas, point);
 
     // create dimension instance
     var dimension;
     // create color instance
     var color = new obelisk.CubeColor().getByHorizontalColor(obelisk.ColorPattern.GRAY);
+    var cubeColor;
 
     // create cube primitive
     //var cube = new obelisk.Cube(dimension, color);
@@ -30,7 +31,7 @@ window.onload=function(){
     //var cube = new obelisk.Cube(dimension, color, false);
 
     //Testing 3d co-ordinate system.
-    var dimension2 = new obelisk.CubeDimension(30, 30, 3);
+    var dimension2 = new obelisk.CubeDimension(20, 20, 3);
     var cube = new obelisk.Cube(dimension2, color, false);
     var p3d = new obelisk.Point3D(0, 0, 0);
     pixelView.renderObject(cube, p3d)
@@ -41,32 +42,48 @@ window.onload=function(){
 	    	p3d = new obelisk.Point3D(x, y, 0)
 	    	// render primitive to view
 	    	pixelView.renderObject(cube, p3d);
-	    	y+=30;
+	    	y+=20;
 		}
 		y = 0;
-		x += 30;
+		x += 20;
 	}
 
 
 	x = 0;
 	y = 0;
 	var z = 0;	//remember to +3 to account for the base.
-	var barSize = 30;
+	var barSize;
 
 	//Setting bar sizes;
 	for(var j = 0; j < 7; j++){
 	    for(var i = 0; i < 4; i++){
-	    	barSize = 20 * arr[i][j];
+	    	if(arr[i][j] == 0){
+	    		barSize = 3;
+	    	}
+	    	else{
+	    		barSize = 20 * arr[i][j];
+	    	}
 
-	    	dimension = new obelisk.CubeDimension(30, 30, barSize);
-		    cube = new obelisk.Cube(dimension, color, false);
+	    	//Setting colors - this would be done dynamically by their 'busiest' day.
+	    	if(arr[i][j] > 5){
+				cubeColor = new obelisk.CubeColor().getByHorizontalColor(obelisk.ColorPattern.BLUE);
+	    	}
+	    	else if(arr[i][j] > 3){
+				cubeColor = new obelisk.CubeColor().getByHorizontalColor(obelisk.ColorPattern.PURPLE);
+	    	}
+	    	else{
+				cubeColor = new obelisk.CubeColor().getByHorizontalColor(obelisk.ColorPattern.PINK);
+	    	}
+
+	    	dimension = new obelisk.CubeDimension(20, 20, barSize);
+		    cube = new obelisk.Cube(dimension, cubeColor, false);
 	    	p3d = new obelisk.Point3D(x, y, 0)
 	    	pixelView.renderObject(cube, p3d);
 
-	    	y+=30;
+	    	y+=20;
 		}
 		y = 0;
-		x += 30;
+		x += 20;
 	}
 
 
