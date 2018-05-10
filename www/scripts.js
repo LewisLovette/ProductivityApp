5ca6f71793476afed7755bc61d1a00a09f68dd55
Lewis Lovette <lewislovette3@hotmail.co.uk>
2018-05-10 09:57:43 +0100

    Clean up + more tests-
    Getting closer but progress is slow

--- www/scripts.js
+++ www/scripts.js
@@ -83,41 +83,16 @@ window.onload=function(){
 	}
 }
 
+//Datepicker stuff
+var $input = $('.datepicker').pickadate()
 
-/**
- * Initialize with current year and date. Returns reference to plugin object.
- */
-var jfcalplugin = $("#mycal").jFrontierCal({
-	date: new Date(),
-	dayClickCallback: myDayClickHandler,
-	agendaClickCallback: myAgendaClickHandler,
-	agendaDropCallback: myAgendaDropHandler,
-	dragAndDropEnabled: true
-}).data("plugin");
-/**
- * Get the date (Date object) of the day that was clicked from the event object
- */
-function myDayClickHandler(eventObj){
-	var date = eventObj.data.calDayDate;
-	alert("You clicked day " + date.toDateString());
-};
-/**
- * Get the agenda item that was clicked.
- */
-function myAgendaClickHandler (eventObj){
-	var agendaId = eventObj.data.agendaId;
-	var agendaItem = jfcalplugin.getAgendaItemById("#mycal",agendaId);
-};
-/**
- * get the agenda item that was dropped. It's start and end dates will be updated.
- */
-function myAgendaDropHandler(eventObj){
-	var agendaId = eventObj.data.agendaId;
-	var date = eventObj.data.calDayDate;		
-	var agendaItem = jfcalplugin.getAgendaItemById("#mycal",agendaId);		
-	alert("You dropped agenda item " + agendaItem.title + 
-		" onto " + date.toString() + ". Here is where you can make an AJAX call to update your database.");
-};
+// Use the picker object directly.
+var picker = $input.pickadate('picker')
 
- jfcalplugin.setAspectRatio("#mycal", 0.75);
+picker.set('select', '2018-05-09', { format: 'yyyy-mm-dd' });
+
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.datepicker');
+	var instances = M.Datepicker.init(elems, options);
+});
 
